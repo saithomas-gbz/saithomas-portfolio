7ed519075e750b4249bab38333f5b02bdd3a9591
Thomas Grare <thomas.grare.pro@gmail.com>
2024-04-19 18:41:54 +0200

    global styles and font

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import { useState } from 'react'
 import Routes from "./config/router";
-import { ThemeProvider } from 'styled-components';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    width: 33.33%;
+    height: 100vh;
+    display: flex; 
+    justify-content: center;
+    position: relative;
+    left: 33.33%;
+    
+  }
+`;
 
 function App() {
   return (
     <>
       {/* <ThemeProvider> */}
-        <Routes />
       {/* </ThemeProvider> */}
+      <GlobalStyle />
+        <Routes />
     </>
   )
 }
