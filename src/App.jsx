6bd4d5941c3941bb3e2f28183cb5807aa4a877b9
saithomas-gbz <thomasgrare64@gmail.com>
2024-05-09 23:20:24 +0200

    theme button design

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { ThemeProvider } from 'styled-components';
 import { lightTheme, darkTheme } from './config/theme';
 import { GlobalStyle } from './shared/GlobalStyle';
 import { ButtonContainer, ThemeButton } from './shared/Button';
-import { TbSun } from "react-icons/tb";
+import { TbSun, TbMoon } from "react-icons/tb";
 
 function App() {
   const [theme, setTheme] = useState('dark');
@@ -19,15 +19,18 @@ function App() {
 
   return (
     <>
-      <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
-      <ButtonContainer>
-        <ThemeButton onClick={toggleTheme}><TbSun size={24}/></ThemeButton>
-      </ButtonContainer>
-      <GlobalStyle />
+      <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+        <ButtonContainer>
+          <ThemeButton onClick={toggleTheme}>
+            {/* <TbSun size={24}/> */}
+            {theme === "light" ? <TbMoon size={24} /> : <TbSun size={24} />}
+          </ThemeButton>
+        </ButtonContainer>
+        <GlobalStyle />
         <Routes />
       </ThemeProvider>
     </>
-  )
+  );
 }
 
 export default App;
